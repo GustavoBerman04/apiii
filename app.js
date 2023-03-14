76fe1aee439e6dc13bcf83f6faaef1ded96d5798
Gustavo Berman <612110430@utsalamanca.edu.mx>
2023-03-13 21:38:44 -0600

    falta el buscar

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
-const naruto = document.getElementById('naruto')
-const narugod = document.getElementById('narugod').content
+const info = document.getElementById('info')
+const tempInfo = document.getElementById('tempInfo').content
 const fragment = document.createDocumentFragment()
 
-let naru = []
+let todo = []
 
-document.addEventListener('DOMContentLoaded', () =>{
-    cargarNaruto()
-})
-
-const cargarNaruto = () => {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'ddb65795bemshb511bd669a7e643p1f5c41jsnc0232d3eba18',
-            'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
-        }
-    };
+const buscarInfo = () => {
     
-    fetch('https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Naruto&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc', options)
-        .then(response => response.json())
-        .then(response => {
-            naru = response.data
-            dibujarNaruto()
-            console.log(response.data)
-        })
-        .catch(err => console.error(err));
+const buscarAnime = document.getElementById('buscarAnime').value
 }
+        document.addEventListener('DOMContentLoaded', () =>{
+            cargarInfo()
+        })
+        
+    
+        const cargarInfo = () => {
+            const encodedParams = new URLSearchParams();
+        encodedParams.append("q", `${buscarAnime.value}`);
+        
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'X-RapidAPI-Key': 'ddb65795bemshb511bd669a7e643p1f5c41jsnc0232d3eba18',
+                'X-RapidAPI-Host': 'anime52.p.rapidapi.com'
+            },
+            body: encodedParams
+        };
+        
+        fetch('https://anime52.p.rapidapi.com/api/search', options)
+            .then(response => response.json())
+            .then(response => {
+                todo = response.results.data
+                //dibujarInfo()
+                console.log(response.results.data)
+            })
+            .catch(err => console.error(err));
+        }
+    
 
-const dibujarNaruto = () => {
-    naru.forEach((item) => {
-        narugod.querySelector('img').setAttribute('src', item.image)
-        narugod.querySelector('h1').textContent = item.title
-        narugod.querySelector('p').textContent = item.synopsis
-        narugod.getElementById('estatus').textContent = ('Estatus: ')+item.status
 
-        const clone = narugod.cloneNode(true)
+
+
+const dibujarInfo = () => {
+    todo.forEach((item) => {
+        tempInfo.querySelector('img').setAttribute('src', item.poster)
+        tempInfo.querySelector('h1').textContent = item.title
+
+        const clone = tempInfo.cloneNode(true)
         fragment.appendChild(clone)
     })
-    naruto.appendChild(fragment)
+    info.appendChild(fragment)
 }
 
